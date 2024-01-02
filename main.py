22146f6a424337d5db777ff9fe809752648139b1
André Medeiros Silva <andre1998vv@gmail.com>
2024-01-02 14:17:39 -0300

    Modificações no main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from Telas .Login .login import *
+from Telas .Menu .menu import *
+from Telas .DadosVendedor .dados_vendedor import *
+from Telas .DadosRemedio .dados_remedio import *
+from Telas .DadosEmpresa .dados_empresa import *
+from Telas .DadosCliente .dados_cliente import *
+from Telas .Login .login import *
+from Telas .CriarConta .criar_conta import *
+from Telas .CadastroVendedor .cadastrar_vendedor import *
+from Telas .CadastroRemedio .cadastrar_remedio import *
+from Telas .CadastroCliente . cadastrar_cliente import *
+
+
+
+class App:
+    def __init__(self):
+        def MudaTela (TelaAtual, ProximaTela):
+            TelaAtual.destroy()
+            ProximaTela.pack
+
+        self.tela_atual = TelaLogin()
+        
+
+
+aplicacao = App()
