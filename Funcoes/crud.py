9cde32351ee3c614fa0f2620576487df1326f2d3
André Medeiros Silva <andre1998vv@gmail.com>
2024-01-07 17:52:36 -0300

    Criação de funções em funcoesdb.py

--- Funcoes/crud.py
+++ Funcoes/crud.py
@@ -8,9 +8,9 @@ cursor = farmacia.cursor()
 
 cursor.execute("CREATE TABLE cliente (cli_nome text, cli_senha text, cli_email text, cli_cep text, cli_cpf text, cli_telefone text, cli_data text, cli_endereco text)")
 
-cursor.execute("CREATE TABLE empresa (emp_nome text, emp_senha text, emp_email text, emp_cnpj)")
 
-cursor.execute("CREATE TABLE vendedor (ven_nome text, ven_senha text, ven_empresa text, ven_cpf text, ven_email text, ven_cep text, ven_telefone text, ven_data text, ven_endereco text)")
+
+
 
 cursor.execute("CREATE TABLE remedio (rem_nome text, rem_empresa text, rem_lote text, rem_tipo text, rem_preco real, rem_marca text)") 
 
